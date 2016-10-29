e6df8ef64beb5136f3ddb7e6cce66d19d1b005c4
natenka <nataliya.samoylenko@gmail.com>
2016-10-29 15:51:16 +0300

    Update exercises

--- exercises/ch_12/task12.4.py
+++ exercises/ch_12/task12.4.py
@@ -5,10 +5,9 @@
 В задании используется пример из раздела про [модуль threading](book/chapter12/5a_threading.md).
 
 Переделать пример таким образом, чтобы:
-* функция connect_ssh не выводила результат выполнения команды на стандартный поток вывода, а возвращала вывод.
-* пример надо изменить так, чтобы в результате выполнение команды на всех устройствах, возвращался словарь, где:
- * ключ - IP-адрес устройства
- * значение - результат вывода команды
+* вместо функции connect_ssh, использовалась функция send_commands из задания 12.3
+ * переделать функцию send_commands, чтобы использовалась очередь и функция conn_threads по-прежнему возвращала словарь с результатами.
+ * Проверить работу со списком команд, с командами из файла, с командой show
 
 '''
 
@@ -16,27 +15,37 @@ from netmiko import ConnectHandler
 import sys
 import yaml
 import threading
+from Queue import Queue
 
 COMMAND = sys.argv[1]
-all_devices = yaml.load(open('devices.yaml'))
+devices = yaml.load(open('devices.yaml'))
 
-def connect_ssh(device_dict, command):
+def connect_ssh(device_dict, command, queue):
+
     ssh = ConnectHandler(**device_dict)
     ssh.enable()
     result = ssh.send_command(command)
-
     print "Connection to device %s" % device_dict['ip']
-    print result
+
+    queue.put({ device_dict['ip']: result })
 
 
 def conn_threads(function, devices, command):
     threads = []
+    q = Queue()
+
     for device in devices:
-        th = threading.Thread(target = function, args = (device, command))
+        th = threading.Thread(target = function, args = (device, command, q))
         th.start()
         threads.append(th)
 
     for th in threads:
         th.join()
 
-conn_threads(connect_ssh, all_devices['routers'], COMMAND)
+    results = []
+    for t in threads:
+        results.append(q.get())
+
+    return results
+
+print conn_threads(connect_ssh, devices['routers'], COMMAND)
