70b8082eabe74d5048765e15baf4bcfa2c06c680
natenka <nataliya.samoylenko@gmail.com>
2017-04-06 19:44:41 +0300

    Update chapter 11 (db) exercises

--- exercises/11_db/task_11_5a/task_11_5a.py
+++ exercises/11_db/task_11_5a/task_11_5a.py
@@ -6,24 +6,23 @@
 После выполнения задания 11.5, в таблице dhcp есть новое поле last_active.
 
 Обновите скрипт add_data.py, таким образом, чтобы он удалял все записи,
-которые были активными более недели назад.
+которые были активными более 7 дней назад.
 
 Для того, чтобы получить такие записи, можно просто вручную обновить поле last_active.
 
 В файле задания описан пример работы с объектами модуля datetime.
-Обратите внимание, что объекты, как и строки с датой, которые мы пишем в БД,
+Обратите внимание, что объекты, как и строки с датой, которые пишутся в БД,
 можно сравнивать между собой.
+
 """
 
 from datetime import timedelta, datetime
 
 now = datetime.today().replace(microsecond=0)
-two_weeks_ago = now - timedelta(days = 14)
+week_ago = now - timedelta(days = 7)
 
 #print now
-#print seven_days_ago
-#print now > seven_days_ago
-#print now.__str__() > seven_days_ago.__str__()
+#print week_ago
+#print now > week_ago
+#print str(now) > str(week_ago)
 
-# now_string нужна для того, чтобы записать время записи в БД в виде строки
-now_string = now.__str__()
